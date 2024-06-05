4fd9dfa5c80212e94fbefd1270907166d35b3605
torabekov-08 <diyorbektorabekov_08@gmail.com>
2024-06-05 08:16:04 +0500

    commit

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import time
 
 import flet as ft
+
+from flet import TextField
+from flet_core.control_event import ControlEvent
+
+
 #
 # def main(page: ft.page):
 #     text = ft.Text(value="My nick name is Torabekov_08",color="blue")
@@ -11,13 +16,43 @@ import flet as ft
 # ft.app(target=main)
 
 
+# def main(page:ft.Page):
+#     text = ft.Text(color="blue")
+#     page.add(text)
+#     for i in range(10):
+#         text.value = f"sleep {i}"
+#         page.update()
+#         time.sleep(1)
+#
+# ft.app(target=main)
 
-def main(page:ft.Page):
-    text = ft.Text(color="blue")
-    page.add(text)
-    for i in range(10):
-        text.value = f"sleep {i}"
+
+def main(page: ft.Page) -> None:
+    page.title = 'increment counter'
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.theme_mode = 'light'
+
+    text_number: TextField = TextField('0', text_align=ft.TextAlign.RIGHT, width=80)
+
+    def decrement(e: ControlEvent) -> None:
+        text_number.value = str(int(text_number.value) - 1)
+
         page.update()
-        time.sleep(1)
 
-ft.app(target=main)
+    def increment(e: ControlEvent) -> None:
+        text_number.value = str(int(text_number.value) + 1)
+        page.update()
+
+    page.add(
+        ft.Row(
+            [ft.IconButton(ft.icons.REMOVE, on_click=decrement),
+             text_number,
+             ft.IconButton(ft.icons.ADD, on_click=increment)
+             ],
+            alignment=ft.MainAxisAlignment.CENTER
+        )
+    )
+
+
+if __name__ == '__main__':
+    ft.app(target=main)
